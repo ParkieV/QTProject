27dee2b8e45c877ba3329b881ec01f4b9752bc2e
ParkieV <st-psk@mail.ru>
2023-10-31 02:18:43 +0300

    Add read, update, delete func on CRUD

--- app/crud/transactions_crud.py
+++ app/crud/transactions_crud.py
@@ -1,42 +1,82 @@
+import logging
 import os
 import sys
 from sqlite3 import Connection
 
 sys.path.append(os.getcwd())
-print(sys.path)
 
 from app.methods.validate_query import validate_query_values
 from app.settings.DBSettings import DBManager
 
+DB_NAME = "financial_transactions_test"
 
-class TransactionsCRUD:
-    DB_NAME = "financial_transactions_test"
+@DBManager.db_transactions_query
+@validate_query_values
+def add_row(values: dict, con: Connection | None = None) -> int:
+    query = f"""
+            INSERT into {DB_NAME}
+                ({", ".join(values)})
+            VALUES
+                ({", ".join(map(str, values.values()))})
+            RETURNING transaction_id;
+        """
+    logging.info(f"SQL query is {query}")
+    if con:
+        result = con.execute(query).fetchone()[0]
+        return result
+    else:
+        raise ConnectionError("Connection to database was failed")
 
-    def __init__(self) -> None:
-        if DBManager.check_connection():
-            pass
-        else:
-            pass
-            # raise ConnectionError("Connect to database was failed.")
+@DBManager.db_transactions_query
+def get_row_by_id(id: int, con: Connection | None = None) -> dict:
+    query = f"""
+        SELECT *
+        FROM {DB_NAME}
+        WHERE transaction_id = {id};
+    """
+    logging.info(f"SQL query is {query}")
+    if con:
+        result = con.execute(query).fetchone()
+        return result
+    else:
+        raise ConnectionError("Connection to database was failed")
 
-    @validate_query_values
-    @DBManager.db_transactions_query
-    @staticmethod
-    def add_row(values: dict, con: Connection | None = None) -> int:
-        insert_attribute = list(values.keys())
-        query = f"""
-                INSERT into {TransactionsCRUD.DB_NAME}
-                    ({", ".join(insert_attribute)})
-                VALUES
-                    ({", ".join(map(str, values.values()))})
-                RETURNING transaction_id;
-            """
-        if con:
-            result = con.execute(query).fetchone()[0]
-            return result
-        else:
-            raise ConnectionError("Connection to database was failed")
-if __name__=="__main__":
-    TransactionsCRUD()
-    TransactionsCRUD.add_row(values={"amount": 100,
-                              "transaction_date": "2021-11-30"})
+@DBManager.db_transactions_query
+@validate_query_values
+def update_row_by_id(values: dict, id: int, con: Connection | None = None) -> int:
+    query = f"""
+            UPDATE {DB_NAME}
+                SET {", ".join(map(lambda k: f"{k} = {values[k]}", values))}
+            WHERE transaction_id = {id}
+            RETURNING transaction_id;
+        """
+    logging.info(f"SQL query is {query}")
+    if con:
+        result = con.execute(query).fetchone()[0]
+        return result
+    else:
+        raise ConnectionError("Connection to database was failed")
+
+@DBManager.db_transactions_query
+def delete_row_by_id(id: int, con: Connection | None = None) -> None:
+    query = f"""
+        DELETE from {DB_NAME}
+        WHERE transaction_id = {id};
+    """
+    if con:
+        con.execute(query)
+    else:
+        raise ConnectionError("Connection to database was failed")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO,
+                        filename="app/db_log.log", filemode="w",
+                        format="%(asctime)s %(levelname)s\t%(message)s")
+    add_row(values={"amount": 100,
+        "transaction_date": "2021-11-30"})
+    print(get_row_by_id(10))
+    update_row_by_id(values={"amount": 101}, id=10)
+    print(get_row_by_id(10))
+    delete_row_by_id(10)
+
